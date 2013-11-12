711d9a92057e35ad1a8afbff76735d37ced16d00
Yi Xin <yxin@redhat.com>
2013-11-12 10:21:05 -0500

    Upload 4 templates

---
+++ js/userprofile.foundation.js
@@ -0,0 +1,34 @@
+function editData(){
+  $('.editbutton').html("<button type='button' style='float:right; margin-left:5px;' class='tiny button' onclick='cancelButton()' >Cancel</button>"+
+    "<button type='button' style='float:right;' class='tiny button' onclick='' >Submit</button>");
+  $('#alias').html("<input  type='text' class='form-control' name='alias' id='aliasinput' value='"+$('#alias').text()+"'/>");
+  $('#email').html("<input  type='text' class='form-control' name='email' id='emailinput' value='"+$('#email').text()+"'/>");
+  $('#office').html("<input  type='text' class='form-control' name='office' id='officeinput' value='"+$('#office').text()+"'/>");
+  $('#phone').html("<input  type='text' class='form-control' name='phone' id='phoneinput' value='"+$('#phone').text()+"'/>");
+}
+
+function cancelButton(){
+  $('.editbutton').html("<button type='button' style='float:right;' class='tiny button' onclick='editData()' >Edit</button>");
+  $('#alias').html($('#aliasinput').val());
+  $('#email').html($('#emailinput').val());
+  $('#office').html($('#officeinput').val());
+  $('#phone').html($('#phoneinput').val());
+}
+
+function filloutdata(){
+  $('#aliaspopout').text($('#alias').text());
+  $('#emailpopout').text($('#email').text());
+  $('#officepopout').text($('#office').text());
+  $('#phonepopout').text($('#phone').text());
+  $('#firstnamepopout').text($('#firstname').text());
+  $('#lastnamepopout').text($('#lastname').text());
+  $('#jobtitlepopout').text($('#jobtitle').text());
+  $('#departmentpopout').text($('#department').text());
+  $('#companypopout').text($('#company').text());
+  $('#managementpopout').text($('#management').text());
+  $('#streetpopout').text($('#street').text());
+  $('#citypopout').text($('#city').text());
+  $('#statepopout').text($('#state').text());
+  $('#postalcodepopout').text($('#postalcode').text());
+  $('#countrypopout').text($('#country').text());
+}
